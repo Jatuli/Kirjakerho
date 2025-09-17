b5c8f0ba8aa034c6757d016b8c52521020f024ee
Jatuli <mepasane@gmail.com>
2025-09-17 11:34:00 +0300

    Ensimmäinen commit

---
+++ db.py
@@ -0,0 +1,36 @@
+import sqlite3
+import os
+from flask import g
+DB_PATH = os.path.join(os.path.dirname(__file__), "database.db")
+SCHEMA_PATH = os.path.join(os.path.dirname(__file__), "schema.sql")
+
+def init_db():
+    con = get_connection()
+    with open(SCHEMA_PATH, "r") as f:
+        con.executescript(f.read())  # suorittaa kaikki CREATE TABLE -komennot
+    con.commit()
+    con.close()
+    
+
+def get_connection():
+    con = sqlite3.connect("database.db")
+    con.execute("PRAGMA foreign_keys = ON")
+    con.row_factory = sqlite3.Row
+    return con
+
+def execute(sql, params=[]):
+    con = get_connection()
+    result = con.execute(sql, params)
+    con.commit()
+    g.last_insert_id = result.lastrowid
+    con.close()
+
+def last_insert_id():
+    return g.last_insert_id    
+    
+
+def query(sql, params=[]):
+    con = get_connection()
+    result = con.execute(sql, params).fetchall()
+    con.close()
+    return result
